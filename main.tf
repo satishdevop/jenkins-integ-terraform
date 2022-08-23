42c166785598d3ffd5491e5ebd7f91f64153e8d7
satishdevop <86150834+satishdevop@users.noreply.github.com>
2022-08-23 15:08:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
+provider "aws" {
+  region = "us-west-2"
+}
 
+resource "aws_instance" "my_amazon" {
+  ami           = "ami-076e3a557efe1aa9c" # This is Comments
+  instance_type = "t2.micro"              // This is also Comments
+  key_name      = "Mumbai_keypair"
+
+  tags = {
+    Name    = "My-Linux-Server"
+    Owner   = "Satish"
+    project = "job"
+  }
+}
